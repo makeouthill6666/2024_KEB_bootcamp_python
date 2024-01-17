87f3dad1ec0e8bc5935b25406d5bbb559f11dc6d
makeouthill6666 <superduperrookie@gmail.com>
2024-01-17 15:52:33 +0900

    v3.1

--- day04.py
+++ day04.py
@@ -166,11 +166,20 @@ import copy
 # for v in sugang.values():
 #     print(v)
 
+
+import random
 drinks_foods = {"위스키" : "초콜릿", "와인": "치즈", "소주": "삽겹살", "고량주" : "양꼬치"}
 drinks_foods_keys=list(drinks_foods)
+# print(drinks_foods)
+print(drinks_foods.pop("위스키"))
+print(drinks_foods)
+print(drinks_foods.remove("위스키"))
+print(drinks_foods)
+print(random.choice(drinks_foods_keys))
+
 while True :
     menu = input(
-        f'다음 술중에 고르세요\n1) {drinks_foods_keys[0]}, 2) {drinks_foods_keys[1]}, 3) {drinks_foods_keys[2]}, 4) {drinks_foods_keys[3]}, 5) 종료 : ')
+        f'다음 술중에 고르세요\n1) {drinks_foods_keys[0]}, 2) {drinks_foods_keys[1]}, 3) {drinks_foods_keys[2]}, 4) {drinks_foods_keys[3]}, 5) 랜덤, 6) 종료 : ')
     if menu == '1':
         print(f'{drinks_foods_keys[0]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[0]]}입니다')
         print()
@@ -184,5 +193,9 @@ while True :
         print(f'{drinks_foods_keys[3]}에 어울리는 안주는 {drinks_foods[drinks_foods_keys[3]]}입니다')
         print()
     elif menu == '5':
+        random_drink= random.choice(drinks_foods_keys)
+        print(f'{random_drink}에 어울리는 안주는 {drinks_foods[random_drink]}입니다')
+        print()
+    elif menu == '6':
         print('다음에 또 오세요')
         break
