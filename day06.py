4168dcaa995dc3bbc20ab92f20da09ac26cbd904
makeouthill6666 <superduperrookie@gmail.com>
2024-01-19 10:21:40 +0900

    v3.7 factorial function

---
+++ day06.py
@@ -0,0 +1,53 @@
+# def change_and_print_global():
+#     print('inside change_and_print_global: ', animal)
+#     animal='wombat'
+#     print('after the change:', animal)
+#
+#     print(change_and_print_global())
+
+# def change_local():
+#     animal ='cat'
+#     print('inside change_local:', animal, id(animal))
+#
+# print (change_local())
+#
+#
+# def change_local():
+#     animal ='cat'
+#     print('inside change_local:', animal, id(animal))
+#
+#     print(animal)
+#
+# def change_local():
+#     animal ='cat'
+#     print('inside change_local:', animal, id(animal))
+#
+#     print(id(animal))
+
+def factorial_repetition(n) -> int:
+    """
+    반복문을 이용한 팩토리얼 함수
+    :param n: 정수, int
+    :return: 팩토리얼값, int
+    """
+    result = 1
+    for i in range (2, n+1):
+        result = result *i
+    return result
+
+def factorial_recursion(n):
+    '''
+    재귀함수를 사용한 팩토리얼 함수
+    :param n: 정수, int
+    :return: function, int
+    '''
+    if n ==1:
+        return n
+    else :
+        return n * factorial_recursion((n-1))
+
+number = (int(input("number : ")))
+
+print(factorial_repetition(number))
+
+print(factorial_recursion(number))
